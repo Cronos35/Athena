5b93bcff24ef52a641d02608f2545935ddcf2b34
Cronos35 <gfranciscleo@gmail.com>
2020-02-09 20:59:20 +0800

    Added swipe detection script

---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    private Vector2 direction;
+    private Vector2 fingerUpPosition;
+    private Vector2 fingerDownPosition;
+
+    private float minSwipeDistance = 20f;
+
+    public static event Action<SwipeData> OnSwipe = delegate { };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+           
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+
+   
+}
